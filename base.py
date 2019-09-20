5e6a72161dfb554e67b7d130b92269cc47acd91f
Danny Baque Rodriguez <42458038+DannyBR9@users.noreply.github.com>
2019-09-20 09:43:21 -0500

    verison jaime

--- base.py
+++ base.py
@@ -8,68 +8,77 @@ from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
 from subprocess import call
 
-def TopoBC():
+def myNetwork():
 
     net = Mininet( topo=None,
                    build=False,
                    ipBase='172.18.7.0/16')
 
-    
-    #Agrega switches
-    s14 = net.addSwitch('s14', cls=OVSKernelSwitch)
-    s13 = net.addSwitch('s13', cls=OVSKernelSwitch)
-    s10 = net.addSwitch('s10', cls=OVSKernelSwitch)
-    s12 = net.addSwitch('s12', cls=OVSKernelSwitch)
-    s11 = net.addSwitch('s11', cls=OVSKernelSwitch)
-    s15 = net.addSwitch('s15', cls=OVSKernelSwitch)
-    s9 = net.addSwitch('s9', cls=OVSKernelSwitch)
+    info( '*** Adding controller\n' )
+    info( '*** Add switches\n')
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch, failMode='standalone')
+    s7 = net.addSwitch('s7', cls=OVSKernelSwitch, failMode='standalone')
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch, failMode='standalone')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
+    s6 = net.addSwitch('s6', cls=OVSKernelSwitch, failMode='standalone')
+    s5 = net.addSwitch('s5', cls=OVSKernelSwitch, failMode='standalone')
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
 
+    info( '*** Add hosts\n')
+    h5 = net.addHost('h5', cls=Host, ip='172.18.0.5', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='172.18.0.4', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='172.18.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='172.18.0.2', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='172.18.0.3', defaultRoute=None)
+    h6 = net.addHost('h6', cls=Host, ip='172.18.0.6', defaultRoute=None)
 
-    #Agregando hosts
-    h1 = net.addHost('h1', cls=Host, ip='172.18.7.1', defaultRoute=None)
-    h3 = net.addHost('h3', cls=Host, ip='172.18.7.2', defaultRoute=None)
-    h5 = net.addHost('h5', cls=Host, ip='172.18.7.3', defaultRoute=None)
-    h6 = net.addHost('h6', cls=Host, ip='172.18.7.4', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='172.18.7.5', defaultRoute=None)
-    h4 = net.addHost('h4', cls=Host, ip='172.18.7.6', defaultRoute=None)
+    info( '*** Add links\n')
+    net.addLink(s1, s2, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(s1, s3, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(s2, s4, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(s3, s5, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(s4, s6, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(s5, s7, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(h1, s4,bw=10, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(h2, s6, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(h3, s6, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(h4, s7, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(h5, s7, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
+    net.addLink(h6, s5, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True)
 
-
-
-
-    #Agregando links
-    net.addLink(s11, s12)
-    net.addLink(s12, s13)
-    net.addLink(s13, s14)
-    net.addLink(s14, s15)
-    net.addLink(s11, s10)
-    net.addLink(s10, h3)
-    net.addLink(s9, h1)
-    net.addLink(s9, h2)
-    net.addLink(s10, s9)
-    net.addLink(s15, h5)
-    net.addLink(s15, h6)
-    net.addLink(s14, h4)
-
-    #Iniciando red
+    info( '*** Starting network\n')
     net.build()
-    #IniciandoControladores
+    info( '*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
 
-    #Iniciando switches
-    net.get('s14').start([])
-    net.get('s13').start([])
-    net.get('s10').start([])
-    net.get('s12').start([])
-    net.get('s11').start([])
-    net.get('s15').start([])
-    net.get('s9').start([])
+    info( '*** Starting switches\n')
+    net.get('s4').start([])
+    net.get('s7').start([])
+    net.get('s3').start([])
+    net.get('s1').start([])
+    net.get('s6').start([])
+    net.get('s5').start([])
+    net.get('s2').start([])
 
-    #Enviar configuracion de switches and hosts
+    info( '*** Post configure switches and hosts\n')
 
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    TopoBC()
+    myNetwork()
+
