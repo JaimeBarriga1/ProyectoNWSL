16da56208471080eb76b67ea639be77c75049054
DannyBR <baquedanny9@gmail.com>
2019-09-19 19:30:03 -0500

    comit danny 2

--- base.py
+++ base.py
@@ -1,26 +1,71 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
 
-class MyTopo( Topo ):
-    "Simple topology example."
+def TopoBC():
 
-    def __init__( self ):
-        "Create custom topo."
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='172.18.7.0/16')
 
-        #Inicia topology
-        Topo.__init__( self )
+    
+    #Agrega switches
+    s14 = net.addSwitch('s14', cls=OVSKernelSwitch)
+    s13 = net.addSwitch('s13', cls=OVSKernelSwitch)
+    s10 = net.addSwitch('s10', cls=OVSKernelSwitch)
+    s12 = net.addSwitch('s12', cls=OVSKernelSwitch)
+    s11 = net.addSwitch('s11', cls=OVSKernelSwitch)
+    s15 = net.addSwitch('s15', cls=OVSKernelSwitch)
+    s9 = net.addSwitch('s9', cls=OVSKernelSwitch)
 
-        # Agrega hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+    #Agregando hosts
+    h1 = net.addHost('h1', cls=Host, ip='172.18.7.1', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='172.18.7.2', defaultRoute=None)
+    h5 = net.addHost('h5', cls=Host, ip='172.18.7.3', defaultRoute=None)
+    h6 = net.addHost('h6', cls=Host, ip='172.18.7.4', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='172.18.7.5', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='172.18.7.6', defaultRoute=None)
 
-        # Agrega links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+    #Agregando links
+    net.addLink(s11, s12)
+    net.addLink(s12, s13)
+    net.addLink(s13, s14)
+    net.addLink(s14, s15)
+    net.addLink(s11, s10)
+    net.addLink(s10, h3)
+    net.addLink(s9, h1)
+    net.addLink(s9, h2)
+    net.addLink(s10, s9)
+    net.addLink(s15, h5)
+    net.addLink(s15, h6)
+    net.addLink(s14, h4)
 
+    #Iniciando red
+    net.build()
+    #IniciandoControladores
+    for controller in net.controllers:
+        controller.start()
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+    #Iniciando switches
+    net.get('s14').start([])
+    net.get('s13').start([])
+    net.get('s10').start([])
+    net.get('s12').start([])
+    net.get('s11').start([])
+    net.get('s15').start([])
+    net.get('s9').start([])
+
+    #Enviar configuracion de switches and hosts
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    TopoBC()
